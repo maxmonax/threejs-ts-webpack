d49b39b5a4099ffc73f3eaa9a6b47a0b8d4f204d
Max <maxmonax@gmail.com>
2023-10-19 15:36:03 +0800

    small refactoring

--- src/game/scenes/SphereScene.ts
+++ src/game/scenes/SphereScene.ts
@@ -1,6 +1,4 @@
 import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import { MyMath } from "../../utils/MyMath";
 import { BasicScene } from "./BasicScene";
 import { DebugGui } from "../debug/DebugGui";
 import { Settings } from "../data/Settings";

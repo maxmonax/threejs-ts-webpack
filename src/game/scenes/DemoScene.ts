1b533904f20582b6e604f12613739ebe9aa33a22
Max <maxmonax@gmail.com>
2024-03-24 23:30:35 +0800

    Update DemoScene.ts

--- src/game/scenes/DemoScene.ts
+++ src/game/scenes/DemoScene.ts
@@ -1,13 +1,6 @@
-import * as THREE from "three";
 import { BasicScene } from "../../core/scene/BasicScene";
 import { DebugGui } from "../debug/DebugGui";
-import { Params } from "../data/Params";
 import { SceneNames } from "./SceneNames";
-import { ComposerRenderer } from "../../core/renderers/ComposerRenderer";
-import { Config } from "../data/Config";
-import { ParticleSystem } from "@/core/effects/ParticleSystem";
-import { ThreeLoader } from "@/utils/threejs/ThreeLoader";
-import { TextureAlias } from "../data/TextureData";
 
 export class DemoScene extends BasicScene {
 
